736cd079dca210247a1f4dd1aa1a15da688a08d2
Asharib Ali <asharibarain4@gmail.com>
2024-03-12 04:40:40 +0500

    update

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -19,26 +19,27 @@ module.exports = {
     },
   },
 
-  // Default network configuration
-  defaultNetwork: "blast-sepolia",
-
-  // Blastscan API configuration
+  // Blastexplorer by Routescan API configuration
+  // https://blastexplorer.io/documentation/recipes/hardhat-verification
   etherscan: {
     apiKey: {
-      blast_sepolia: "blast_sepolia",
+      blast: "blast", // apiKey is not required, just set a placeholder
     },
     customChains: [
       {
-        network: "blast_sepolia",
-        chainId: 168587773,
+        network: "blast",
+        chainId: 81457,
         urls: {
           apiURL:
-            "https://api.routescan.io/v2/network/testnet/evm/168587773/etherscan",
-          browserURL: "https://testnet.blastscan.io",
+            "https://api.routescan.io/v2/network/mainnet/evm/81457/etherscan",
+          browserURL: "https://blastexplorer.io",
         },
       },
     ],
   },
+
+  // Blastscan by Etherscan API Configuration
+  // https://docs.blastscan.io/v/blastscan/
 
   // Sourcify integration configuration
   sourcify: {
@@ -47,12 +48,27 @@ module.exports = {
 
   // Network configuration
   networks: {
+    // for mainnet
+    "blast-mainnet": {
+      url: "https://rpc.blast.io/",
+      accounts: [process.env.PRIVATE_KEY],
+      gasPrice: 1000000000,
+    },
+    // for Sepolia testnet
     "blast-sepolia": {
       url: "https://sepolia.blast.io",
       accounts: [process.env.PRIVATE_KEY],
       gasPrice: 1000000000,
     },
+    // for local dev environment
+    "blast-local": {
+      url: "http://localhost:8545",
+      accounts: [process.env.PRIVATE_KEY],
+      gasPrice: 1000000000,
+    },
   },
+  // Default network configuration
+  defaultNetwork: "blast-sepolia",
 
   // Paths for various Hardhat directories
   paths: {
